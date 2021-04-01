e41e62830049b1aa6834b29401a25a8dfa3664fc
mrWD <lvigtor@gmail.com>
2021-04-02 01:49:59 +0600

    first commit

---
+++ assets/js/set-image.js
@@ -0,0 +1,23 @@
+AFRAME.registerComponent('set-image', {
+  schema: {
+    on: { type: 'string' },
+    target: { type: 'selector' },
+    src: { type: 'string' },
+    dur: { type: 'number', default: 300 },
+  },
+
+  init: function () {
+    const data = this.data;
+    const el = this.el;
+
+    el.addEventListener(data.on, function () {
+      setTimeout(function () {
+        if (!data.src) {
+          history.back();
+        } else {
+          window.location.assign(`/${data.src}`);
+        }
+      }, data.dur);
+    });
+  },
+});
